899356c2576586f966c4adb335c8d15d5368fc40
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2021-01-06 10:13:11 -0300

    Project created

---
+++ src/main/java/br/com/jopaulo/jpdelivery/JpdeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.jopaulo.jpdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpdeliveryApplication.class, args);
+	}
+
+}
